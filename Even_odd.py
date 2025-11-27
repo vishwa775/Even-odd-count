356902b9e77169abe3ec3906a0e477916d37b988
vishwa775 <vishwaamagi@gmail.com>
2025-11-27 14:57:07 +0530

    Create Even_odd.py

---
+++ Even_odd.py
@@ -0,0 +1,14 @@
+import sys
+if len(sys.argv) < 2:
+    print("Please provide numbers as command-line arguments.")
+    sys.exit(1)
+numbers = list(map(int, sys.argv[1:]))
+even_count = 0
+odd_count = 0
+for num in numbers:
+    if num % 2 == 0:
+        even_count += 1
+    else:
+        odd_count += 1
+print("Count of even numbers:", even_count)
+print("Count of odd numbers:", odd_count)
